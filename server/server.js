a234ff5e9a81549b78427ebbf068209a60709a09
ConstanceDesign <info@constance.design>
2022-12-16 12:51:29 -0500

    port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // const env = process.env.NODE_ENV || "development";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 // Session
 const sess = {
